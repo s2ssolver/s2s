dab4d074483bde2a980bdeff3371214772e3e9e7
Kevin Lotz <kel@informatik.uni-kiel.de>
2023-04-10 15:43:31 +0200

    Fix cardinalaty encoding type error

--- src/encode/card.rs
+++ src/encode/card.rs
@@ -1,18 +1,19 @@
-use crate::sat::{neg, Cnf, PLit, PVar};
+use crate::sat::{as_lit, neg, Cnf, PLit, PVar};
 
-pub fn exactly_one(lits: &[PLit]) -> Cnf {
+pub fn exactly_one(lits: &[PVar]) -> Cnf {
     eo_naive(lits)
 }
 
 /// Naive encoding of at least one and at most one constraints.
 /// Needs quadratic number of clauses.
-fn eo_naive(list: &[PLit]) -> Cnf {
+fn eo_naive(vars: &[PVar]) -> Cnf {
     let mut cnf = Cnf::new();
     // At least one
-    cnf.push(list.to_vec());
+    let as_lits: Vec<PLit> = vars.iter().copied().map(as_lit).collect();
+    cnf.push(as_lits.clone());
     // At most one
-    for (i, x) in list.iter().enumerate() {
-        for y in list.iter().skip(i + 1) {
+    for (i, x) in as_lits.iter().enumerate() {
+        for y in as_lits.iter().skip(i + 1) {
             cnf.push(vec![-(*x), -(*y)]);
         }
     }
