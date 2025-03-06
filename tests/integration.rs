d8b29968794410d01184ca1628ea0942647cdaad
Kevin Lotz <kel@informatik.uni-kiel.de>
2025-03-06 15:20:21 +0000

    Unsat integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -20,3 +20,21 @@ fn test_sat(smt: &str) {
         Err(err) => panic!("{}", err),
     }
 }
+
+#[test_resources("res/tests_unsat/*.smt2")]
+fn test_unsat(smt: &str) {
+    let file = Path::new(smt);
+    let smt = std::io::BufReader::new(std::fs::File::open(file).unwrap());
+    let mut mngr = NodeManager::default();
+    let script = parse_script(smt, &mut mngr).unwrap();
+    let mut interpreter = Interpreter::new(SolverOptions::default(), &mut mngr);
+
+    for a in script.iter_asserts() {
+        interpreter.assert(a);
+    }
+
+    match interpreter.check_sat() {
+        Ok(res) => assert!(res.is_unsat()),
+        Err(err) => panic!("{}", err),
+    }
+}
