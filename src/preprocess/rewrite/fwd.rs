7973991558e3bc24d9e47e2766c0366b70f95593
Kevin Lotz <kel@informatik.uni-kiel.de>
2025-04-01 11:53:48 +0000

    Move forward reasoning to own file

---
+++ src/preprocess/rewrite/fwd.rs
@@ -0,0 +1,119 @@
+use indexmap::IndexSet;
+
+use crate::{
+    interval::Interval,
+    node::{Node, NodeKind, NodeManager},
+    preprocess::rewrite::int::{normalize_ineq, LinearIntRealtion},
+};
+
+use super::EquivalenceRule;
+
+/// Compares a (linear) integer (in)equalities with an asserted fact.
+/// If both have the same left-hand side, it checks if they are conflicting or if one implies the other.
+/// If they are conflicting, it returns false.
+/// If the fact implies the other, it returns true.
+/// Otherwise, it returns None.
+/// This is used to simplify integer (in)equalities in the forward rewriting.
+#[derive(Debug, Clone, Copy)]
+pub(super) struct LinIntForward;
+
+impl LinIntForward {
+    fn apply(fact: &Node, other: &Node, mngr: &mut NodeManager) -> Option<Node> {
+        let fact_norm = normalize_ineq(fact)?;
+        let other_norm = normalize_ineq(other)?;
+        // We check if they are
+        // - conflicting: `fact` and `other` cannot be true at the same time. In that case, we return false.
+        // - valid: `fact` implies `other`. In that case, we return true.
+        // - otherwise, we return None.
+
+        /// Compores two inequalities of the form:
+        /// - `LHS op1 r1`
+        /// - `LHS op2 r2`
+        /// If they are conflicting, it returns false.
+        /// If the the first implies the second, it returns true.
+        /// Otherwise, it returns None.
+        fn compare(fact: LinearIntRealtion, other: LinearIntRealtion) -> Option<bool> {
+            fn to_interval(rel: &LinearIntRealtion) -> Vec<Interval> {
+                let op = rel.op();
+                let r = rel.rhs();
+                let interval = match op {
+                    NodeKind::Lt => Interval::bounded_above(r - 1),
+                    NodeKind::Le => Interval::bounded_above(r),
+                    NodeKind::Eq => Interval::new(r, r),
+                    NodeKind::Ge => Interval::bounded_below(r),
+                    NodeKind::Gt => Interval::bounded_below(r + 1),
+                    _ => unreachable!(),
+                };
+                if rel.pol() {
+                    vec![interval]
+                } else {
+                    // If the polarity is negative, we compute the complement of the interval [l, u]
+                    // Left: (-inf, l-1]
+                    // Right: [u+1, +inf)
+                    let left = Interval::bounded_above(interval.lower() - 1.into());
+                    let right = Interval::bounded_below(interval.upper() + 1.into());
+                    vec![left, right]
+                }
+            }
+
+            let fact_interval = to_interval(&fact);
+            let other_interval = to_interval(&other);
+
+            let mut disjoint = true;
+            let mut subsumed = true;
+            for left in fact_interval {
+                for right in &other_interval {
+                    // Check if the intervals are disjoint
+                    disjoint &= left.intersect(*right).is_empty();
+                    // Check if the fact is a subset of the other
+                    subsumed &= left.is_subset(*right);
+                }
+            }
+            debug_assert!(!disjoint || !subsumed);
+            if disjoint {
+                return Some(false);
+            }
+            if subsumed {
+                return Some(true);
+            }
+            None
+        }
+
+        if fact_norm.lhs() == other_norm.lhs() {
+            if compare(fact_norm, other_norm)? {
+                return Some(mngr.ttrue());
+            } else {
+                return Some(mngr.ffalse());
+            }
+        }
+
+        None
+    }
+}
+
+impl EquivalenceRule for LinIntForward {
+    fn apply(
+        &self,
+        node: &Node,
+        asserted: &IndexSet<Node>,
+        mngr: &mut NodeManager,
+    ) -> Option<Node> {
+        match node.kind() {
+            NodeKind::Lt
+            | NodeKind::Le
+            | NodeKind::Gt
+            | NodeKind::Ge
+            | NodeKind::Eq
+            | NodeKind::Not => {
+                for fact in asserted.iter().filter(|a| *a != node) {
+                    if let Some(equiv) = LinIntForward::apply(fact, node, mngr) {
+                        return Some(equiv);
+                    }
+                }
+            }
+            _ => (),
+        }
+
+        None
+    }
+}
--- src/preprocess/rewrite/strlen.rs
+++ src/preprocess/rewrite/strlen.rs
@@ -204,3 +204,125 @@ impl EquivalenceRule for LengthToReg {
         None
     }
 }
+
+/// Finds asserted terms of the form `L = 0` or `L <= 0`, where
+/// all terms in L are of the form c|x| with c>0
+/// and returns `x = epsilon` for all c|x| in L.`
+#[derive(Clone, Default, Debug)]
+pub(super) struct ZeroLengthEpsilon;
+
+impl ZeroLengthEpsilon {
+    fn apply(lhs: &Node, mngr: &mut NodeManager) -> Option<VarSubstitution> {
+        // Collects all in the given node of sort string that need to be replaced by epsilon
+        // Returns false if a variable is found that is not of sort string
+        fn collect_vars(n: &Node, in_len: bool, vars: &mut IndexSet<Rc<Variable>>) -> bool {
+            match n.kind() {
+                NodeKind::Length => {
+                    assert!(!in_len);
+                    for c in n.children() {
+                        // must be true since the length of a string is a string
+                        collect_vars(c, true, vars);
+                    }
+                    true
+                }
+                NodeKind::Concat => {
+                    assert!(in_len);
+                    for c in n.children() {
+                        // must be true since we can only concatenate strings
+                        collect_vars(c, in_len, vars);
+                    }
+                    true
+                }
+                NodeKind::Variable(v) if in_len => {
+                    // must be a string, we are in a length
+                    assert!(v.sort().is_string());
+                    vars.insert(v.clone());
+                    true
+                }
+                NodeKind::Mul | NodeKind::Add => {
+                    assert!(!in_len);
+                    for c in n.children() {
+                        if !collect_vars(c, in_len, vars) {
+                            // If we find a variable that is not of sort string, we can't apply the rule
+                            // and return false
+                            return false;
+                        }
+                    }
+                    true
+                }
+                NodeKind::Int(i) => *i >= 0, // if we find a negative integer, we can't apply the rule
+                NodeKind::Sub => false,
+                NodeKind::Variable(v) => {
+                    debug_assert!(!in_len);
+                    debug_assert!(v.sort().is_int());
+                    // if we find a variable that is not of sort string, we can't apply the rule
+                    false
+                }
+                _ => false,
+            }
+        }
+        let mut vars = IndexSet::new();
+        if collect_vars(lhs, false, &mut vars) && !vars.is_empty() {
+            // set all variables to epsilon
+            let mut subs = VarSubstitution::default();
+            let epsi = mngr.empty_string();
+            for v in vars {
+                subs.add(v, epsi.clone());
+            }
+            Some(subs)
+        } else {
+            None
+        }
+    }
+}
+
+impl EntailmentRule for ZeroLengthEpsilon {
+    fn apply(
+        &self,
+        node: &Node,
+        asserted: &IndexSet<Node>,
+        _: bool,
+        mngr: &mut NodeManager,
+    ) -> Option<VarSubstitution> {
+        // This is only applicable if the node itself is asserted
+        if !asserted.contains(node) {
+            return None;
+        }
+
+        match node.kind() {
+            NodeKind::Eq => {
+                debug_assert!(node.children().len() == 2);
+                let lhs = node.children().first().unwrap();
+                let rhs = node.children().last().unwrap();
+                if lhs.sort().is_int() && rhs.sort().is_int() {
+                    match (lhs.kind(), rhs.kind()) {
+                        (_, NodeKind::Int(0)) => return ZeroLengthEpsilon::apply(lhs, mngr),
+                        (NodeKind::Int(0), _) => return ZeroLengthEpsilon::apply(rhs, mngr),
+                        _ => (),
+                    }
+                }
+            }
+            NodeKind::Le => {
+                debug_assert!(node.children().len() == 2);
+                let lhs = node.children().first().unwrap();
+                let rhs = node.children().last().unwrap();
+                match (lhs.kind(), rhs.kind()) {
+                    (_, NodeKind::Int(0)) => return ZeroLengthEpsilon::apply(lhs, mngr),
+                    _ => (),
+                }
+            }
+            NodeKind::Ge => {
+                debug_assert!(node.children().len() == 2);
+                let lhs = node.children().first().unwrap();
+                let rhs = node.children().last().unwrap();
+                match (lhs.kind(), rhs.kind()) {
+                    (NodeKind::Int(0), _) => return ZeroLengthEpsilon::apply(rhs, mngr),
+                    _ => (),
+                }
+            }
+            _ => (),
+        }
+        None
+    }
+}
+
